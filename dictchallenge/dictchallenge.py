ad1f69dede3369980d75cf5bd4ba38528187a559
David Shon <jeong.shon0219@gmail.com>
2021-06-08 16:59:14 +0000

    copying local files with python

---
+++ dictchallenge/dictchallenge.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python3
+
+char_name= input(Which character do you want to know about? (Wolverine)
+
+char_stat= input(What statistic do you wnat to know about?)
+
+
+heroes=  {
+"wolverine":
+    {"real name": "James Howlett",
+    "powers": "regeneration",
+    "archenemy": "Sabertooth",},
+"harry potter":
+    {"real name": "Harry Potter",
+    "powers": "magic",
+    "archenemy": "Voldemort",},
+"captain america":
+    {"real name": "Steve Rogers",
+    "powers": "frisbee shield",
+    "archenemy": "Hydra",}
+        }
+
