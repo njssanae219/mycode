ddafbeba4f0c872c5192409c2160498368d12f26
David Shon <jeong.shon0219@gmail.com>
2021-06-10 21:19:06 +0000

    python and excel, including

---
+++ 3dstem.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+theta = np.linspace(0, 2*np.pi)
+x = np.cos(theta - np.pi/2)
+y = np.sin(theta - np.pi/2)
+z = theta
+
+fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
+ax.stem(x, y, z)
+
+#plt.show()
+plt.savefig("/home/student/static/3dstem.png")
---
+++ readingcsvproj/readingcsvproj.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import csv
+
+with open("pokedex.txt", "r") as csvfile:
+    
+
