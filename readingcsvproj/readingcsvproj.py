8a1e7e2d17c6b53b07cb2768a168c600acb4d942
David Shon <jeong.shon0219@gmail.com>
2021-06-11 16:14:07 +0000

    lab37 os.walk() the dir tree

--- readingcsvproj/readingcsvproj.py
+++ readingcsvproj/readingcsvproj.py
@@ -2,6 +2,19 @@
 
 import csv
 
-with open("pokedex.txt", "r") as csvfile:
+def randomLine():
+
+    with open("pokedex.txt", "r") as csvfile:
+        pokefile= csv.reader(csvfile, delimiter=",")
+        name = input("Type in the name of your favorite pokemon character: ").lower()
+
+        for row in pokefile:
+            if name in row[1].lower(): # checks for similar name
+                filename= "pokedex2.txt" #overwrites the files
+
+                with open(filename, "w") as pkfile:
+                    print(f"{row[1]}", file=pkfile)
+
+randomLine()
     
 
