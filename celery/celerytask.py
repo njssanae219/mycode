19f7ec7a4eac1fd9e5be17a4a3f2cc0220621945
David Shon <jeong.shon0219@gmail.com>
2021-06-18 14:41:07 +0000

    self study proj_integrate celery with flask

---
+++ celery/celerytask.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from flask import Flask
+from celerytask import celery1
+
+app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'amqps://xicbpsnc:Sr32LS7_SAxgHvjpRBJDamQDX4n2M2nE@fish.rmq.cloudamqp.com/xicbpsnc'
+app.config['CELERY_BACKEND'] = 'amqps://xicbpsnc:Sr32LS7_SAxgHvjpRBJDamQDX4n2M2nE@fish.rmq.cloudamqp.com/xicbpsnc'
+
+celery = celery1(app)
+
+@app.route('/process/<name>')
+def process(name):
+    return name
+
+@celery.task(name='celeryconfig.reverse')
+def reverse(string):
+    return string[::-1]
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0", port=2224)
